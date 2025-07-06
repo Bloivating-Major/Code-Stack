bb4065c15c8e6dfe7a86d3df768af300534a5cc2
Bloivating-Major <sambhav12wakhariya@gmail.com>
2025-07-05 23:30:00 -0400

    Checked In : Toaster Added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,10 +66,30 @@ const config: Config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "toast-slide-in-right": {
+          from: { transform: "translateX(calc(100% + 1rem))" },
+          to: { transform: "translateX(0)" },
+        },
+        "toast-slide-out-right": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(100% + 1rem))" },
+        },
+        "toast-swipe-out": {
+          from: { transform: "translateX(var(--radix-toast-swipe-end-x))" },
+          to: { transform: "translateX(calc(100% + 1rem))" },
+        },
+        "toast-fade": {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "toast-slide-in-right": "toast-slide-in-right 0.3s ease-in-out",
+        "toast-slide-out-right": "toast-slide-out-right 0.3s ease-in-out",
+        "toast-swipe-out": "toast-swipe-out 0.3s ease-in-out",
+        "toast-fade": "toast-fade 0.3s ease-in-out",
       },
     },
   },
